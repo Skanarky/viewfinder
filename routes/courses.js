2576136c5db9eccc82abc6003893d970d8b8c410
Skanarky <kutkurov@gmail.com>
2018-05-08 17:45:12 -0600

    Added ENV VARs

--- routes/courses.js
+++ routes/courses.js
@@ -5,7 +5,7 @@ const coursesRouter = express.Router();
 const access = {
     auth: {
         username: "PRXxcMDxIV9Z4GoO5KhCeKC4WwI5XSEwjuygaNpW",
-        password: "PV7SFPRv1SqE84Vh27QCGk3A7o1Y82pjP5lxMDMYFiNL1DYz0nrV2QCSeKIMJDCptqKOeYqFQnCAIPfcnAQaDa94sq0MlyXqxfmkjWsvS9mhb6fi5WooTJZgOqDX4S9r"
+        password: process.env.UDEMY_PASS
     }
 };
 
@@ -18,13 +18,8 @@ coursesRouter.route("/")
 
 coursesRouter.route("/:searchKey")
     .get((req, res) => {
-        axios.get(`https://www.udemy.com/api-2.0/courses/?search=photography+${req.params.searchKey}&page_size=40`, {
-            auth:
-                {
-                    username: "PRXxcMDxIV9Z4GoO5KhCeKC4WwI5XSEwjuygaNpW",
-                    password: "PV7SFPRv1SqE84Vh27QCGk3A7o1Y82pjP5lxMDMYFiNL1DYz0nrV2QCSeKIMJDCptqKOeYqFQnCAIPfcnAQaDa94sq0MlyXqxfmkjWsvS9mhb6fi5WooTJZgOqDX4S9r"
-                }
-        }).then(response => res.status(200).send(response.data.results))
+        axios.get(`https://www.udemy.com/api-2.0/courses/?search=photography+${req.params.searchKey}&page_size=40`, access)
+        .then(response => res.status(200).send(response.data.results))
             .catch(() => res.status(404).send());
     });
 
