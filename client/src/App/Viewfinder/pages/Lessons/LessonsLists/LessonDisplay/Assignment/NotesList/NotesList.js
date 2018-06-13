e21ce5909be5247f3fa838a6fe1923b9bf1574ec
Skanarky <kutkurov@gmail.com>
2018-06-13 17:20:52 -0600

    added dates in notes

--- client/src/App/Viewfinder/pages/Lessons/LessonsLists/LessonDisplay/Assignment/NotesList/NotesList.js
+++ client/src/App/Viewfinder/pages/Lessons/LessonsLists/LessonDisplay/Assignment/NotesList/NotesList.js
@@ -3,8 +3,13 @@ import React from "react";
 class NotesList extends React.Component {
 
     render = () => {
+        const months = ["Jan", "Feb", "Mar", "Apr",
+            "May", "Jun", "July", "Aug",
+            "Sep", "Oct", "Nov", "Dec"]
         const { note, errMsgNotes, loadingNotes, deleteNote } = this.props;
         // console.log(note);
+        const date = new Date(note.createdAt);
+        console.log(typeof date);
 
         if (loadingNotes) {
             return (
@@ -16,11 +21,11 @@ class NotesList extends React.Component {
             )
         } else {
             return (
-                    <li className="oneNote">
-                        <p>{note.textNote}</p>
-                        <h6>Added on: {note.createdAt}.getDate()</h6>
-                        <button style={{height: "35px"}} onClick={() => deleteNote(note._id)}>Delete Note</button>  
-                    </li>
+                <li className="oneNote">
+                    <p>{note.textNote}</p>
+                    <h6>Added on {months[date.getMonth()]} {date.getDate()}, at {date.getHours()}:{date.getMinutes()}:{date.getSeconds()}</h6>
+                    <button style={{ height: "35px" }} onClick={() => deleteNote(note._id)}>Delete Note</button>
+                </li>
             )
         }
     }
