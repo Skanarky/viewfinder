9cad1f75ae4e2a850dbae62c190a0b9f0ff72661
Skanarky <kutkurov@gmail.com>
2019-10-15 13:27:29 -0600

    fix name prcy

--- client/src/App/FooterPrvcy/FooterPrvcy.js
+++ client/src/App/FooterPrvcy/FooterPrvcy.js
@@ -17,7 +17,7 @@ class FooterPrvcy extends React.Component {
             return (
                 <div className="ftr-prvcy">
                     <button className="ftr-prvcy-btn-close" onClick={this.clBanner}>X</button>&nbsp;&nbsp;&nbsp; This application is using YouTube API Services. By using the application or closing this banner, you acknowledge that you have read and agree to &nbsp;
-                    <Link to="http://www.google.com/policies/privacy" target="_blank">Google Privacy Policy</Link> and <Link to="https://www.youtube.com/static?template=terms" target="_blank">YuoTube Terms of Service</Link>&nbsp;
+                    <Link to="http://www.google.com/policies/privacy" target="_blank">Google Privacy Policy</Link> and <Link to="https://www.youtube.com/static?template=terms" target="_blank">YouTube Terms of Service</Link>&nbsp;
                     <button className="ftr-prvcy-btn-gotit" onClick={this.clBanner}>Got It!</button>
                 </div>
             )
